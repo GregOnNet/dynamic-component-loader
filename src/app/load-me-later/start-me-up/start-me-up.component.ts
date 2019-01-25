462b5f0b849afa5a8f4836dc50955fba16144ba4
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-25 09:55:51 +0100

    demonstrate cleanup with destroy$$

--- src/app/load-me-later/start-me-up/start-me-up.component.ts
+++ src/app/load-me-later/start-me-up/start-me-up.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-start-me-up',
   templateUrl: './start-me-up.component.html',
   styleUrls: ['./start-me-up.component.scss']
 })
-export class StartMeUpComponent implements OnInit {
+export class StartMeUpComponent implements OnInit, OnDestroy {
+  destroy$$ = new Subject();
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.data.subscribe(data =>
-      console.log('Resolved', data.resolvedRoles)
-    );
+    this.route.data
+      .pipe(takeUntil(this.destroy$$))
+      .subscribe(data => console.log('Resolved', data.resolvedRoles));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$$.next();
+    this.destroy$$.complete();
   }
 }
