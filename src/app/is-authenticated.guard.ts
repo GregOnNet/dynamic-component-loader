dc3d42e11dabbeda620825a75d72a8b50ab2296f
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-25 09:41:34 +0100

    add redirect demo in guard

--- src/app/is-authenticated.guard.ts
+++ src/app/is-authenticated.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot
 } from '@angular/router';
 
@@ -9,14 +10,23 @@ import {
   providedIn: 'root'
 })
 export class IsAuthenticatedGuard implements CanActivate {
+  constructor(private _router: Router) {}
+
   canActivate(
     _next: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot
   ): boolean {
-    return window.confirm(
+    const decision = window.confirm(
       `Sure to navigate? Allowed Roles are: ${_next.data.permissions.only.join(
         ', '
       )}`
     );
+
+    if (!decision) {
+      // this._router.navigate(['main', 'admin', 2313]); // main/admin/2313;
+      this._router.navigateByUrl('');
+    }
+
+    return decision;
   }
 }
